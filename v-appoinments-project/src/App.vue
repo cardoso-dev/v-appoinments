a8804f13e1078f5d36fc722106bba21089ebd288
pedro <pedro@microbit.com>
2022-06-07 08:13:20 -0500

    Add json file as source data

--- v-appoinments-project/src/App.vue
+++ v-appoinments-project/src/App.vue
@@ -1,44 +1,23 @@
 <template>
   <PersonAppoinments
     personName="Dentist Jhon Doe"
-    :appoinments="{
-      'Jun-09':
-        {
-          '09:00 am': {
-            'patient': 'Laura Martinez',
-            'notes': 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate '
-          },
-          '10:00 am': {
-            'patient': 'Kendra Rodriguez',
-            'notes': 'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo'
-          },
-          '04:00 pm': {
-            'patient': 'Monica Smith',
-            'notes': 'hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat'
-          }
-        },
-      'Jun-10':
-        {
-          '08:00 am': {
-            'patient': 'Abril White',
-            'notes': 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate '
-          },
-          '10:30 am': {
-            'patient': 'Williams Frederick',
-            'notes': 'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo'
-          }
-        }
-    }"
+    :appoinments=dataAppoiments
     />
 </template>
 
 <script>
-import PersonAppoinments from './components/PersonAppoinments.vue'
+import PersonAppoinments from './components/PersonAppoinments.vue';
+import dataJson from './data/data.json';
 
 export default {
   name: 'App',
   components: {
     PersonAppoinments
+  },
+  data(){
+    return {
+      dataAppoiments: dataJson
+    }
   }
 }
 </script>
